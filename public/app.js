26435157d2a0ab72d8e9a77fd35f8a18c526f0ad
namtonnest <142704723+namtonnest@users.noreply.github.com>
2024-05-06 12:10:57 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('https://proapp-ek13.onrender.com')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
